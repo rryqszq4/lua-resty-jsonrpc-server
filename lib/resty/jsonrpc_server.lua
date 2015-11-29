02312ff3cfba295fc908c799d5cbc9577ecd4430
rryqszq4 <279664262@163.com>
2015-11-29 22:57:03 +0800

    [server] add function interface

---
+++ lib/resty/jsonrpc_server.lua
@@ -0,0 +1,45 @@
+-- Copyright (c) 2015, rryqszq4
+-- All rights reserved.
+
+cjson = require "cjson"
+
+local _jsr_server = {
+	_VERSION = '0.0.1'
+}
+
+function _jsr_server.new()
+
+end
+
+function _jsr_server.register()
+end
+
+function _jsr_server.bind()
+end
+
+function _jsr_server.jsonformat()
+end
+
+function _jsr_server.rpcformat()
+end
+
+function _jsr_server.executeprocedure()
+end
+
+function _jsr_server.executecallback()
+end
+
+function _jsr_server.executemethod()
+end
+
+function _jsr_server.getresponse()
+end
+
+function _jsr_server.execute()
+end
+
+function _jsr_server.destroy()
+end
+
+return _jsr_server
+
