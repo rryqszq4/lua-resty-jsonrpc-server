7ff3fd1fbc1500f4cf7a9879d7ef1778ab181d75
rryqszq4 <279664262@163.com>
2016-01-18 16:03:04 +0800

    [server] format request

--- lib/resty/jsonrpc_server.lua
+++ lib/resty/jsonrpc_server.lua
@@ -15,9 +15,9 @@ function _M.new(self)
 	local callbacks
 	local classes
 
-	ngx.req.read_body()
-
-	payload = cjson.decode(ngx.var.request_body)
+	--ngx.say(ngx.var.request_body);
+	--payload = cjson.decode(ngx.var.request_body)
+	payload = nil
 	callbacks = {}
 	
 	return 
@@ -36,10 +36,53 @@ end
 function _M.bind()
 end
 
-function _M.json_format()
+function _M.json_format(self)
+	if self.payload == nil then
+		ngx.req.read_body()
+		self.payload = ngx.var.request_body
+	end
+
+	if type(self.payload) ==  "string" then
+		self.payload = cjson.decode(self.payload)
+	end
+
+	if type(self.payload) == "table" then
+		return true
+	else
+		return false
+	end
 end
 
-function _M.rpc_format()
+function _M.rpc_format(self)
+	if type(self.payload) ~= "table" then
+		return {
+			-32600, 
+			"Invalid Request"
+		}
+	end
+
+	if self.payload["jsonrpc"] == nil or self.payload["jsonrpc"] ~= "2.0" then
+		return {
+			-32600,
+			"Invalid Request"
+		}
+	end
+
+	if self.payload["method"] == nil or type(self.payload["method"]) ~= "string" then
+		return {
+			-32601,
+			"Method not found"
+		}
+	end
+
+	if self.payload["params"] == nil or type(self.payload["params"]) ~= "table" then
+		return {
+			-32602,
+			"Invalid params"
+		}
+	end
+
+	return nil
 end
 
 function _M.execute_procedure()
@@ -47,20 +90,54 @@ end
 
 function _M.execute_callback(self)
 	local method = self.callbacks[self.payload.method]
-	local success, result = pcall(method, unpack(self.payload.params))
-	return result
+	if method ~= nil then
+		local success, result = pcall(method, unpack(self.payload.params))
+		return self:get_response(result)
+	else 
+		return self:rpc_error(-32601, "Method not found")
+	end
 end
 
 function _M.execute_method()
 end
 
-function _M.get_response()
+function _M.get_response(self, data)
+	return cjson.encode({
+		jsonrpc = "2.0",
+		id = "null",
+		result = data
+	})
 end
 
 function _M.execute(self)
+	local result
 
-	local result = self:execute_callback()
-	return result
+	result = self:json_format()
+	if result ~= true then
+		return self:rpc_error(-32700, "Parse error")
+	end
+
+	result = self:rpc_format()
+	if result ~= nil then
+		return self:rpc_error(result[1], result[2])
+	end
+
+	result = self:execute_callback()
+	ngx.say(result)
+
+	--local result = self:execute_callback()
+	--return result
+end
+
+function _M.rpc_error(self, code, message)
+	return cjson.encode({
+		jsonrpc = "2.0",
+		error = {
+			code = code,
+			message = message
+		},
+		id = "null"
+	})
 end
 
 function _M.destroy()
